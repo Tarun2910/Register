4e37d0d0ef39ae06966f8d851eccb6f9503d6ba7
unknown <tarun@TARUN-CC.costacloud.in>
2024-05-24 17:34:46 +0530

    Proxy updated

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,31 +3,31 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/tenants', {
-      target: 'http://gateway-test.apps.ocp4.pacosta.com',
+      target: 'http://3.7.32.64:9090',
       changeOrigin: true,
     }),
   );
   app.use(
     createProxyMiddleware('/auth', {
-      target: 'http://gateway-test.apps.ocp4.pacosta.com',
+      target: 'http://3.7.32.64:9090',
       changeOrigin: true,
     }),
   );
   app.use(
     createProxyMiddleware('/multitenant', {
-      target: 'http://gateway-test.apps.ocp4.pacosta.com',
+      target: 'http://3.7.32.64:9090',
       changeOrigin: true,
     }),
   );
   app.use(
     createProxyMiddleware('/dms_service', {
-      target: 'http://gateway-test.apps.ocp4.pacosta.com',
+      target: 'http://3.7.32.64:9090',
       changeOrigin: true,
     }),
   );
   app.use(
     createProxyMiddleware('/crm', {
-      target: 'http://gateway-test.apps.ocp4.pacosta.com',
+      target: 'http://3.7.32.64:9090',
       changeOrigin: true,
     }),
   );
