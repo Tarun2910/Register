a28e8a6d587dc55f2bb8e23178f5322e491276cd
Mayank Tanwar <mayank@costacloud.com>
2024-07-16 11:31:25 +0530

    user search

---
+++ src/redux/features/usersDashboardSlice.js
@@ -0,0 +1,142 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {
+  createAsyncThunkWithTokenRefresh,
+  createAxiosConfig,
+} from '../common/commonFunctions';
+import axios from 'axios';
+import {toast} from 'react-toastify';
+
+const initialState = {
+  usersData: {},
+  usersDataIsLoading: false,
+  usersDataIsError: false,
+  usersDataError: '',
+  usersDataIsSuccess: false,
+
+  applicationsData: {},
+  applicationsDataIsLoading: false,
+  applicationsDataIsError: false,
+  applicationsDataError: '',
+  applicationsDataIsSuccess: false,
+};
+
+export const getUsersData = createAsyncThunkWithTokenRefresh(
+  'users/tableData',
+  async (token, currentUser, payload) => {
+    try {
+      console.log(payload);
+      let url =
+        payload.searchText == ''
+          ? `/tenants/users`
+          : `/tenants/users?keyword=${payload.searchText}&pageNum=${payload.pageNumber}`;
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          searchText: payload.searchText,
+          appName: payload.applicationName,
+          pageNumber: payload.pageNumber,
+        },
+        maxBodyLength: Infinity,
+      });
+      console.log(response.data);
+      return response;
+    } catch (e) {
+      console.log(e);
+    }
+  },
+);
+
+export const getApplicationsData = createAsyncThunkWithTokenRefresh(
+  'users/applicationsData',
+  async (token, currentUser, payload) => {
+    try {
+      const response = await axios.get('/tenants/applications', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          username: localStorage.getItem('username'),
+          searchText: payload.searchText,
+        },
+        maxBodyLength: Infinity,
+      });
+      return response;
+    } catch (e) {
+      console.log(e);
+    }
+  },
+);
+
+export const usersDataboardSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    setStorageDataPageNumber(state, action) {
+      state.pageNumber = action.payload;
+    },
+
+    resetUsersDashboard(state) {
+      state.usersData = {};
+      state.usersDataIsLoading = false;
+      state.usersDataIsError = false;
+      state.usersDataError = '';
+      state.usersDataIsSuccess = false;
+    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(getUsersData.pending, (state) => {
+        state.usersData = {};
+        state.usersDataIsLoading = true;
+        state.usersDataIsError = false;
+        state.usersDataError = '';
+        state.usersDataIsSuccess = false;
+      })
+      .addCase(getUsersData.fulfilled, (state, action) => {
+        console.log(action.payload);
+        state.usersData = action.payload;
+        state.usersDataIsLoading = false;
+        state.usersDataIsError = false;
+        state.usersDataError = '';
+        state.usersDataIsSuccess = true;
+      })
+      .addCase(getUsersData.rejected, (state, action) => {
+        state.usersData = {};
+        state.usersDataIsLoading = false;
+        state.usersDataIsError = true;
+        state.usersDataError = action.error.message;
+        state.usersDataIsSuccess = false;
+        toast(action.error.message, {autoClose: 2000, type: 'error'});
+      })
+      .addCase(getApplicationsData.pending, (state) => {
+        state.applicationsData = {};
+        state.applicationsDataIsLoading = true;
+        state.applicationsDataIsError = false;
+        state.applicationsDataError = '';
+        state.applicationsDataIsSuccess = false;
+      })
+      .addCase(getApplicationsData.fulfilled, (state, action) => {
+        console.log(action.payload);
+        state.applicationsData = action.payload;
+        state.applicationsDataIsLoading = false;
+        state.applicationsDataIsError = false;
+        state.applicationsDataError = '';
+        state.applicationsDataIsSuccess = true;
+      })
+      .addCase(getApplicationsData.rejected, (state, action) => {
+        state.applicationsData = {};
+        state.applicationsDataIsLoading = false;
+        state.applicationsDataIsError = true;
+        state.applicationsDataError = action.error.message;
+        state.applicationsDataIsSuccess = false;
+        toast(action.error.message, {autoClose: 2000, type: 'error'});
+      });
+  },
+});
+
+export const {resetUsersDashboard, setStorageDataPageNumber} =
+  usersDataboardSlice.actions;
+
+export default usersDataboardSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import {setupListeners} from '@reduxjs/toolkit/query';
 import authReducer from './features/authSlice';
 import usersReducer from './features/usersSlice';
 import teamSyncReducer from './features/teamSyncSlice';
+import usersDashboardReducer from './features/usersDashboardSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: usersReducer,
     teamSync: teamSyncReducer,
+    usersDashboard: usersDashboardReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
